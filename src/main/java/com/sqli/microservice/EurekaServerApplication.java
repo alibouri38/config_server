a77bf13f4b49ad5ac2f6087dc7f4de47ec217577
alibouri38 <137088679+alibouri38@users.noreply.github.com>
2023-07-10 17:58:55 +0100

    2nd commit

--- src/main/java/com/sqli/microservice/EurekaServerApplication.java
+++ src/main/java/com/sqli/microservice/EurekaServerApplication.java
@@ -2,7 +2,6 @@ package com.sqli.microservice;
 
 import org.springframework.boot.SpringApplication;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
